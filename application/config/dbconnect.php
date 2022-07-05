13cb1edbb4f5f692bde4bb466facadb7ce6d6aaa
jhabdulrahmanzs <abdulrahman.jawad@zucisystems.com>
2022-07-05 19:04:08 +0530

    local

--- application/config/dbconnect.php
+++ application/config/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $conn = mysqli_connect('localhost', 'root', '','corephp_mvc');
-if (!$conn) {
-    die('Could not connect: ' . mysqli_error());
-}
-echo 'Connected successfully';
+// if (!$conn) {
+//     die('Could not connect: ' . mysqli_error());
+// }
+// echo 'Connected successfully';
 //mysqli_close($conn);
 ?>
