51f64fc82b5e288cc3d13657d0d640e90cce00f1
jhabdulrahmanzs <abdulrahman.jawad@zucisystems.com>
2022-07-06 12:47:07 +0530

    fixing bugs

--- application/controller/RegisterController.php
+++ application/controller/RegisterController.php
@@ -1,16 +1,29 @@
 <?php
  
- include('../config/dbconnect.php');
- include('../views/register.php');
+ include '../config/dbconnect.php';
+ include '../views/register.php';
 
- 
+ session_start();
+
+
 //  function submit(){
 
   if(isset($_POST['reg_submit']))  {
     $username = $_POST['username'];
     $useremail = $_POST['useremail'];
     $userpwd = $_POST['userpwd'];
+    $_SESSION['username'] = $username;
     $sql = 'insert into register(username,useremail,userpwd) values("'.$username.'","'.$useremail.'","'.$userpwd.'")';
+    
+
+    // $uname = $_POST ["username"];  
+    // if (!preg_match ("/^[a-zA-z]*$/", $uname) ) {  
+    //     $ErrMsg = "Only alphabets and whitespace are allowed.";  
+    //             echo $ErrMsg;  
+    // } else {  
+    //     echo $uname;  
+    // }  
+
     if(mysqli_query($conn, $sql)){
       header("Location: http://localhost/corephp_mvc/application/views/home.php");
  
