c70c54a5ace50c1164d4e6d17410f919ef756613
Pravin Murugandi <pravin.murugandi@zucisystems.com>
2022-07-06 12:43:16 +0530

    lcoas

--- application/controller/LoginController.php
+++ application/controller/LoginController.php
@@ -5,44 +5,26 @@ include('../config/dbconnect.php');
 include('../views/login.php');
 if(isset($_POST['Login']))
 {
-
-    $username=$_POST['username'];
-    $_SESSION['username'] = $username;
-
+    // $username=$_POST['username'];
+    // $_SESSION['username'] = $username;
+    $useremail=$_POST['useremail'];
+    $_SESSION['useremail']=$useremail;
     // print_r($username);
-
     $userpwd=$_POST['userpwd'];
-
     // print_r($userpwd);
-
-    $query=" SELECT * from register where username='$username' AND userpwd='$userpwd'";
-
-    // print_r($query);
-
+    $query=" SELECT * from register where   useremail='$useremail' AND userpwd='$userpwd'";
+   // print_r($query);
     $connect=mysqli_query($conn,$query);
-
     $result=mysqli_num_rows($connect);
-
     // print_r($result);
-
     if($result==0)
-
     {
-
         echo"<script>alert('password or email is wrong')</script>";
-
         exit();
-
     }
-
     else{
-
         echo"<script>alert('you have loggied in successfully')</script>";
-
         echo"<script>window.open('http://localhost/corephp_mvc/application/views/home.php','_self')</script>";
-
     }
 
 }
-
-?>
--- application/views/login.php
+++ application/views/login.php
@@ -1,40 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-		<meta charset="utf-8">
-		<title>Login</title>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-        
-        <link href="../../src/css/login.css" rel="stylesheet" type="text/css">
-	</head>
-    <title>login</title>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+    <link href="../../src/css/login.css" rel="stylesheet" type="text/css">
+</head>
+<title>login</title>
+
 <body>
     <div class="login">
-    <form action="http://localhost/corephp_mvc/application/controller/LoginController.php" method="post">
+        <form action="http://localhost/corephp_mvc/application/controller/LoginController.php" method="post">
+            <label for="useremail">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="useremail" placeholder="useremail" id="useremail" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="userpwd" placeholder="Password" id="password" required>
+            <input type="submit" value="Login" name="Login">
+            <p><a href="register.php">register here</a></p>
 
-<label for="username">
-
-    <i class="fas fa-user"></i>
-
-</label>
-
-<input type="text" name="username" placeholder="username" id="username" required>
-
-<label for="password">
-
-    <i class="fas fa-lock"></i>
-
-</label>
-
-<input type="password" name="userpwd" placeholder="Password" id="password" required>
-
-<input type="submit" value="Login" name ="Login">
-
-
-
-<p><a href="register.php">register here</a></p>
-
-</form>
+        </form>
     </div>
 </body>
+
 </html>
